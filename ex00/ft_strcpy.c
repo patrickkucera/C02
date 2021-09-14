26fdb2619fe3dd6b2289402f94ff24bafbb03d33
pakucera <pakucera@student.42lausanne.ch>
2021-09-15 00:16:01 +0200

    c02/ex00 new commit

--- ex00/ft_strcpy.c
+++ ex00/ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: pakucera <pakucera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 22:13:55 by pakucera          #+#    #+#             */
-/*   Updated: 2021/09/15 00:11:44 by pakucera         ###   ########.fr       */
+/*   Updated: 2021/09/15 00:15:45 by pakucera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*ft_strcpy(char *dest, char *src)
 	return (dest);
 }
 
-int	main(void)
+/*int	main(void)
 {
 	char	dest[255] = "lasdoadsdas";
 	char	src[255] = "sourci";
@@ -32,4 +32,4 @@ int	main(void)
 	//printf("Avant test = %s\n", dest);
 	ft_strcpy(dest, src);
 	//printf("Apres test = %s\n", dest);
-}
+}*/
