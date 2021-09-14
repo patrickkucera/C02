123852705199cee34003aba57ab8c97a48319f56
pakucera <pakucera@student.42lausanne.ch>
2021-09-15 00:09:23 +0200

    new commit

--- ex00/ft_strcpy.c
+++ ex00/ft_strcpy.c
@@ -6,41 +6,30 @@
 /*   By: pakucera <pakucera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 22:13:55 by pakucera          #+#    #+#             */
-/*   Updated: 2021/09/14 23:31:05 by pakucera         ###   ########.fr       */
+/*   Updated: 2021/09/15 00:08:16 by pakucera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/* char	*mystrcpy( char *destination, const char *source )
-{
-	char	*returnValue = destination;
-    while( *destination++ = *source++);
-    return returnValue;
-}
- */
-#include <stdio.h>
 
 char	*ft_strcpy(char *dest, char *src)
 {
 	int	i;
-	i = 0;
 
-	while(src[i] != '\0') 
+	i = 0;
+	while (src[i] != '\0')
 	{
-		//dest = src;
-		printf("%c\n", src[i]);
-		printf("%c\n", dest[i]);
+		dest[i] = src[i];
 		i++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
 
 int	main(void)
 {
-	char dest[255] = "Un long texte";
-	char src[255] = "Bla autre texte";
+	char dest[255] = "lasdoadsdas";
+	char src[255] = "sourci";
 	
-	printf("Avant dest = %s\n", dest);
+	printf("Avant test = %s\n", dest);
 	ft_strcpy(dest, src);
-	printf("Apres dest = %s\n", dest);
-	
+	printf("Apres test = %s\n", dest);
 }
