f3379bd95c22e9756a0b57ac7531d7a8ec4b662c
pakucera <pakucera@student.42lausanne.ch>
2021-09-15 10:49:31 +0200

    c02-ex02

--- ex02/ft_str_is_alpha.c
+++ ex02/ft_str_is_alpha.c
@@ -1,39 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pakucera <pakucera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 22:13:55 by pakucera          #+#    #+#             */
-/*   Updated: 2021/09/15 10:14:41 by pakucera         ###   ########.fr       */
+/*   Updated: 2021/09/15 10:49:10 by pakucera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-char	*ft_strncpy(char *dest, char *src, unsigned int n)
+int	ft_str_is_alpha(char *str)
 {
-	unsigned int	i;
+	int	i;
 
 	i = 0;
-	while (src[i] != '\0' && i < n)
+	while (str[i])
 	{
-		dest[i] = src[i];
-		i++;
+		if (str[i] < 'a' && str[i] > 'z' || str[i] < 'A' && str[i] > 'Z')
+		{
+			return (0);
+			printf("imprimer avant return 1 :%s", str);
+		}
+		else
+		{
+			return (1);
+		}
 	}
-	while (i < n)
-	{
-		dest[i] != '\0';
-		i++;
-	}
-	return (dest);
-}	
+}
 
-/*int	main(void)
+/* int main(void)
 {
-	char a[]	= "abcd";
-	char b[]	= "1234";
-	printf("%s", ft_strncpy(a, b, 3));
+	char str[] = "abcd";
+	printf("Doit retourner 1 : %s\n", ft_str_is_alpha);
+	ft_str_is_alpha(str);
 	return (0);
-}*/
+} */
