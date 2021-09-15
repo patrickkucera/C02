ea395c60e8dfc269d301f228b46472f000a45fb1
pakucera <pakucera@student.42lausanne.ch>
2021-09-15 07:59:00 +0200

    c02-ex01

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: pakucera <pakucera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 22:13:55 by pakucera          #+#    #+#             */
-/*   Updated: 2021/09/15 07:45:49 by pakucera         ###   ########.fr       */
+/*   Updated: 2021/09/15 07:58:26 by pakucera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,15 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	int	i;
 
 	i = 0;
-	while (i != '\0')
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src [i];
 		ii+;
 	}
+	while (i < n)
+	{
+		dest[i] != '\0';
+		i++;
+	}
+	return (dest);
 }
