d8a4c11077a668e735c9e8b05198dd126bd77d13
pakucera <pakucera@student.42lausanne.ch>
2021-09-18 20:46:57 +0200

    new commit c02ex09

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -6,13 +6,13 @@
 /*   By: pakucera <pakucera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/18 19:28:02 by pakucera          #+#    #+#             */
-/*   Updated: 2021/09/18 20:16:50 by pakucera         ###   ########.fr       */
+/*   Updated: 2021/09/18 20:46:37 by pakucera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-char	*ft_strcapitalize(char *str)
+char	*ft_strlowcase(char *str)
 {
 	int	i;
 
@@ -25,19 +25,35 @@ char	*ft_strcapitalize(char *str)
 		}
 		i++;
 	}
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] < 48 || str[i] > 122)
-		{
-			str[i] -= 32;
-		}
-	}
-	i++;
 	return (str);
 }
 
-/*int	main(void)
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+	int	start;
+
+	i = 0;
+	start = 1;
+	ft_strlowcase(str);
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (start == 1)
+				str[i] -= 32;
+			start = 0;
+		}
+		else if (str[i] >= '0' && str[i] <= '9')
+			start = 0;
+		else
+			start = 1;
+		i++;
+	}
+	return (str);
+}
+
+int	main(void)
 {
 	char str1[] = "Bonjour Comment";	
 	char str2[] = "42lausanne lausanne";
@@ -59,4 +75,3 @@ char	*ft_strcapitalize(char *str)
 	printf("str4: %s\n", str4);
 	return (0);
 }
-*/
